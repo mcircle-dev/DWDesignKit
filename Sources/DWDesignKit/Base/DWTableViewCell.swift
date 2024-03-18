bb1dbd4561c368b67846741f1bb0519eb4cddda0
eunbkang-ids <eunbkang@idstrust.com>
2024-03-18 15:19:42 +0900

    feature MCCWC-3836 #comment Base 테이블뷰셀, 컬렉션뷰셀 추가

---
+++ Sources/DWDesignKit/Base/DWTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  DWTableViewCell.swift
+//
+//
+//  Created by Eunbee Kang on 3/18/24.
+//
+
+import UIKit
+
+open class DWTableViewCell: UITableViewCell {
+    
+    override public init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        configViewHierarchy()
+        configLayoutConstraints()
+        configViewSettings()
+    }
+    
+    @available(*, unavailable)
+    required public init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    open func configViewHierarchy() { }
+    open func configLayoutConstraints() { }
+    open func configViewSettings() { }
+}
